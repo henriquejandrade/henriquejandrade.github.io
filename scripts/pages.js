872d24c6a66a1ccfd7aa18935460f2651a447323
Henrique Andrade <henriqueandrade_@hotmail.com>
2020-03-23 07:38:09 -0300

    "Historia" index page appearance touches

--- scripts/pages.js
+++ scripts/pages.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     //     $(this).width($(this).parent().height());
     // });
 
-    $('.button, .clickable').click(function (event) {
+    $('.button, .clickable, .nav-button').click(function (event) {
         // Navigation
         if ($(this).attr("href")) {
             switch (event.which) {

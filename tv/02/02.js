ce472b5931ed5504aba8bd4d165c6e29ef22a9cd
Henrique Andrade <henriqueandrade_@hotmail.com>
2020-04-14 08:28:26 -0300

    Timer test 2

--- tv/02/02.js
+++ tv/02/02.js
@@ -6,7 +6,9 @@ var buttonStart;
 var timer;
 var totalTime = 60;
 var timeCount = 0;
+
 var isRunning = false;
+var isInterval = false;
 
 $(document).ready(function () {
     timeCount = 0;
@@ -16,10 +18,44 @@ $(document).ready(function () {
     $('.button#pause').hide();
     $('.button#stop').hide();
     $('.button#start').show();
+
+    $('#div-clock').hide();
+
+    initCanvas();
 });
 
 function tick() {
-    setClock(timeCount);
+    // Checks if time is over
+    if (!isInterval) {
+        if (timeCount >= totalTime) {
+            isInterval = true;
+
+            timeCount = 0;
+            totalTime = 60 * parseInt($('#interval-time').val());
+
+            $('#clock').toggleClass('interval');
+
+            // Animate interval show title
+            showIntervalTitle();
+
+            // Animate background new box
+            drawBox();
+        }
+    } else {
+        if (timeCount >= totalTime) {
+            isInterval = false;
+
+            timeCount = 0;
+            totalTime = 60 * parseInt($('#block-time').val());
+
+            $('#clock').toggleClass('interval');
+
+            // Animate interval hide title
+            hideIntervalTitle();
+        }
+    }
+
+    setClock(timeCount + 1);
     timeCount++;
 }
 
@@ -30,19 +66,25 @@ function setClock(time) {
 
 function start() {
     if (!isRunning) {
+        totalTime = 60 * parseInt($('#block-time').val());
         isRunning = true;
 
         $('.button#pause').show();
         $('.button#stop').show();
         $('.button#start').hide();
 
-        timer = window.setInterval(tick, 1000);
+        $('#div-clock-inputs').hide();
+        $('#div-clock').show();
+        $('#div-clock #clock-title').text('');
+    } else {
+        $('.button#pause').show();
+        $('.button#start').hide();
     }
+
+    timer = window.setInterval(tick, 1000);
 }
 
 function pause() {
-    isRunning = false;
-
     $('.button#pause').hide();
     $('.button#stop').show();
     $('.button#start').show();
@@ -60,5 +102,62 @@ function stop() {
     $('.button#stop').hide();
     $('.button#start').show();
 
+    $('#div-clock-inputs').show();
+    $('#div-clock').hide();
+
+    $('#clock').removeClass('interval');
+
     window.clearInterval(timer);
+}
+
+// Typing Animation
+
+var intervalTitle = "Interval";
+var intervaTitleIndex = 0;
+var intervalTitleTimer;
+
+function showIntervalTitle() {
+    intervaTitleIndex = 1;
+    intervalTitleTimer = window.setInterval(tickIntervalTitleShow, 50);
+}
+
+function hideIntervalTitle() {
+    intervaTitleIndex = intervalTitle.length;
+    intervalTitleTimer = window.setInterval(tickIntervalTitleHide, 50);
+}
+
+function tickIntervalTitleShow() {
+    $('#clock-title').text(intervalTitle.substring(0, intervaTitleIndex));
+    intervaTitleIndex++;
+
+    if (intervaTitleIndex > intervalTitle.length) {
+        window.clearInterval(intervalTitleTimer);
+    }
+}
+
+function tickIntervalTitleHide() {
+    $('#clock-title').text(intervalTitle.substring(0, intervaTitleIndex));
+    intervaTitleIndex--;
+
+    if (intervaTitleIndex < 0) {
+        window.clearInterval(intervalTitleTimer);
+    }
+}
+
+// Background animation
+var canvasContext;
+var backgroundBoxIndex;
+
+function initCanvas() {
+    canvasContext = document.getElementById('bgCanvas').getContext('2d');
+    backgroundBoxIndex = 0;
+}
+
+function drawBox() {
+    canvasContext.fillStyle = "#DED2FF";
+    var i = 32 * (backgroundBoxIndex % 8);
+    var j = 32 * Math.trunc(backgroundBoxIndex / 8);
+    canvasContext.fillRect(i, j, 32, 32);
+
+    backgroundBoxIndex++;
 }

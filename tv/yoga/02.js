e0a396acd25d1f1c1a755fd52765c81f603e8760
Henrique Andrade <henriqueandrade_@hotmail.com>
2020-04-20 22:02:08 -0300

    Yoga Update Fix 4

--- tv/yoga/02.js
+++ tv/yoga/02.js
@@ -23,8 +23,6 @@ $(document).ready(function () {
 
     $('#div-clock').hide();
 
-    initBgAnimation();
-
     blip = new Audio('blip.wav');
 });
 
@@ -165,70 +163,4 @@ function tickIntervalTitleHide() {
     if (intervaTitleIndex < 0) {
         window.clearInterval(intervalTitleTimer);
     }
-}
-
-// Background animation
-// var domBody;
-// var bgAnimTimer;
-// var sun;
-// var moon;
-
-// var colorMidnight = [10, 0, 83];
-// var colorMorning = [115, 185, 255];
-// var colorAfternoon = [77, 122, 255];
-// var colorNight = [0, 0, 64];
-// var colorLate = [26, 0, 102];
-
-function initBgAnimation() {
-    // domBody = $('body');
-    // sun = $('#sun');
-    // moon = $('#moon');
-
-    // domBody.css('background-image', 'url()');
-    // refreshBackground();
-    // bgAnimTimer = window.setInterval(refreshBackground, 10000);
-
-    // Turns dim off
-    // $("#dim-div").animate({
-    //     opacity: 0
-    // });
-}
-
-function refreshBackground() {
-    // var now = new Date();
-    // var nowSeconds = now.getHours() * 60 * 60 +
-    //     now.getMinutes() * 60 +
-    //     now.getSeconds();
-
-    // var borderColorA;
-    // var borderColorB;
-
-    // // What time is it?
-    // if (nowSeconds < 10800) {
-    //     borderColorA = colorMidnight;
-    //     borderColorB = colorLate;
-    // } else if (nowSeconds < 32400) {
-    //     borderColorA = colorLate;
-    //     borderColorB = colorMorning;
-    // } else if (nowSeconds < 54000) {
-    //     borderColorA = colorMorning;
-    //     borderColorB = colorAfternoon;
-    // } else if (nowSeconds < 75600) {
-    //     borderColorA = colorAfternoon;
-    //     borderColorB = colorNight;
-    // } else {
-    //     borderColorA = colorNight;
-    //     borderColorB = colorMidnight;
-    // }
-
-    // var dimColor = [myLerp(borderColorA[0], borderColorB[0], nowSeconds / 86400),
-    // myLerp(borderColorA[1], borderColorB[1], nowSeconds / 86400),
-    // myLerp(borderColorA[2], borderColorB[2], nowSeconds / 86400)];
-
-    // domBody.css('background-color', 'rgb(' + dimColor[0] + ',' + dimColor[1] + ',' + dimColor[2] + ')');
-
-    // Sun and Moon positions
-    // var position = myLerp(-50, 150, nowSeconds / 86400);
-    // sun.css('bottom', position + '%');
-    // moon.css('top', (position - 100) + '%');
 }
